06457df471c53bb77df1fc2c895909594007499f
Tom Wilson <twilson63@gmail.com>
2021-08-13 16:19:43 +0000

    bug: update was returning false when successful (#1)

---
+++ scripts/hyper.js
@@ -0,0 +1,11 @@
+import { default as hyper } from "https://x.nest.land/hyper@1.4.3/mod.js";
+import { default as app } from "https://x.nest.land/hyper-app-opine@1.2.1/mod.js";
+import { default as dndb } from "../mod.js";
+
+hyper({
+  app,
+  adapters: [{
+    port: "data",
+    plugins: [dndb()],
+  }],
+});
